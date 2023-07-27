b2e0cc6a2b70e37a8b48dd784e9a449e93dac875
Dizza <Dizzardi@mail.ru>
2023-07-28 01:06:25 +0500

    Переделка 8 задания

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 function seasons() {
     let answerUser = Number(prompt("Введите номер месяца (от 1 до 12) и вы узнаете, к какому времени года он относится:"));
 
-    if(isNaN(answerUser) || answerUser===""|| answerUser<1 || answerUser>12) {
-        alert("Что-то пошло не так!Введите корректное значение.");
-
-    }switch(answerUser) {
-        case 1:
-      case 2:
-      case 12:
-        alert(`Зима`);
-        break;
-      case 3:
-      case 4:
-      case 5:
-        alert(`Весна`);
-        break;
-      case 6:
-      case 7:
-      case 8:
-        alert(`Лето`);
-        break;
-      case 9:
-      case 10:
-      case 11:
-        alert(`Осень`);
-        break;
-      default:
-        break;
+    if(answerUser == 12 || answerUser >= 1 && answerUser <=2) {
+      alert("Зима");
     }
-    
+    else if (answerUser >=3 && answerUser <=5) {
+      alert("Весна");
+    }
+    else if (answerUser >=6 && answerUser <=8) {
+      alert("Лето");
+    }
+    else if (answerUser >= 9 && answerUser <= 11) {
+      alert("Осень")
+    }
+    else {
+      alert("Что то пошло не так!Введите подходящие значение и попробуйте снова!")
+    }
 }
-
+seasons()
